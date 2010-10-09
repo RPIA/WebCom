039fef52fded4c0cbe3b2b79ff99f429beea35f3
Peter <ragonep@gmail.com>
2010-10-09 19:14:49 -0400

    more info!

--- global/user.php
+++ global/user.php
@@ -42,8 +42,10 @@ function login($username, $password) {
         return 3;
     }
 
+    $result = std_query("SELECT * FROM `users` WHERE `userID`='".$try_user['userID']."'")
     // this person checks out, log them in
     $_SESSION = $try_user;
+    $_SESSION['info'] = mysql_fetch_assoc($result);
     $_SESSION['logged_in'] = 1;
 
 	// code for good login
