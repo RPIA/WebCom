42514f73ef2c449421249f12e35d49cdcc5b0613
Michael O'Keefe <okeefm@rpi.edu>
2010-10-18 12:50:21 -0400

    Added basic login stuff to nightcrews

---
+++ nightcrews.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("./global/includes.php");
+
+display_header("Night Crews", "");
+
+if (logged_in()) {
+	// form to change profile info
+	echo "Night Crew information goes here";
+}
+
+else if (!logged_in()) {
+  echo "Can't change anything, 'cause you're not logged in";
+}
+
+display_footer();
+
+?>
