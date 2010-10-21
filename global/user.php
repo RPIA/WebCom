7c095c59f15bbbf062fa6af10a818bc49af96f78
Peter <ragonep@gmail.com>
2010-10-21 19:23:40 -0400

    Signed-off-by: Peter <ragonep@gmail.com>

--- global/user.php
+++ global/user.php
@@ -45,9 +45,9 @@ function login($username, $password) {
     // get user info
     $result = std_query("SELECT * FROM `users` INNER JOIN `states` ON `states`.`id`=`users`.`addressState` WHERE `users`.`id`='".$try_user['userID']."'");
     $userInfo = mysql_fetch_assoc($result);
-    $result = std_query("SELECT `desc` FROM `memberStatuses` INNER JOIN `memberStatusAssociations` ON `memberStatuses`.`id`=`memberStatusAssociations`.`memberStatus` INNER JOIN `users` ON `users`.`id`=`memberStatusAssociations`.`member_id` WHERE `users`.`id`='".$userInfo['majorID']."'");
+    $result = std_query("SELECT `id`,desc` FROM `memberStatuses` INNER JOIN `memberStatusAssociations` ON `memberStatuses`.`id`=`memberStatusAssociations`.`memberStatus` INNER JOIN `users` ON `users`.`id`=`memberStatusAssociations`.`member_id` WHERE `users`.`id`='".$userInfo['majorID']."'");
 	while ($row = mysql_fetch_assoc($result))
-		$memberStatusInfo[] = $row['desc'];
+		$memberStatusInfo[$row['id']] = $row['desc'];
     // get major info
     $result = std_query("SELECT *,`majors`.`id` AS `majorID`, `majors`.`name` AS `majorName`, `majorSchools`.`name` AS `schoolName` FROM `majors` INNER JOIN `majorSchools` ON `majorSchools`.`id`=`majors`.`schoolID` WHERE `majors`.`id`='".$userInfo['majorID']."'");
     $majorInfo = mysql_fetch_assoc($result);
--- profile.php
+++ profile.php
@@ -9,7 +9,11 @@ include_once("./global/includes.php");
 display_header("Profile", "");
 
 if (logged_in()) {
+	$userInfo = $_SESSION['userInfo'];
 	// form to change profile info
+	?>
+<h2><?php echo $userInfo['firstName'];?> <?php echo $userInfo['lastName'];?></h2>
+	<?php
 	echo "You'll be able to change your login info here!";
 	echo "<pre>";
 	print_r($_SESSION);
