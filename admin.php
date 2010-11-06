55ab56c403de5920c9e6e7dbc925da64d628dbc6
Nick Schulte <theschulte@gmail.com>
2010-11-05 21:58:23 -0400

    wrote up a skeleton for the admin panel

--- admin.php
+++ admin.php
@@ -8,7 +8,38 @@ include_once('./global/includes.php');
 display_header("Administrator Panel", "");
 
 if (check_admin_user()) {
-	echo "Welcome, administrator.";
+	switch ($_GET['status']) {
+		case 'adduser':
+			break;
+		case 'edituser':
+			break;
+		case 'deluser':
+			break;
+		case 'addevent':
+			break;
+		case 'editevent':
+			break;
+		case 'delevent':
+			break;
+		case 'editcrew':
+			break;
+		case 'editdefault':
+			break;
+			
+		default:
+			echo "<p align='center'>";
+			echo "<a href='admin.php?status=adduser'>Add User</a><br />";
+			echo "<a href='admin.php?status=edituser'>Edit User</a><br />";
+			echo "<a href='admin.php?status=deluser'>Delete User</a><br />";
+			echo "<br />";
+			echo "<a href='admin.php?status=addevent'>Add Event</a><br />";
+			echo "<a href='admin.php?status=editevent'>Edit Event</a><br />";
+			echo "<a href='admin.php?status=delevent'>Delete Event</a><br />";
+			echo "<br />";
+			echo "<a href='admin.php?status=editcrew'>Edit Night Crew</a><br />";
+			echo "<a href='admin.php?status=editdefault'>Edit Default Night Crew</a><br />";
+			echo "</p>";
+	}
 }
 
 else {
