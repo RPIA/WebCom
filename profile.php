06cd00bba7dceb2d2cbfb4e48baa98b1af633513
Peter <ragonep@gmail.com>
2010-11-18 15:05:29 -0500

    Signed-off-by: Peter <ragonep@gmail.com>

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@ if (logged_in()) {
 	$userInfo = $_SESSION['userInfo'];
 	// form to change profile info
 	?>
-<span><span style="size:18pt;font-weight:bold;"><?php echo $userInfo['firstName'];?> <?php echo $userInfo['lastName'];?></span>
+<span><span style="size:18pt;font-weight:bold;"><?php echo $userInfo['firstName'];?> <?php echo $userInfo['lastName'];?></span><br>
 <span style="size:14pt;font-weight:bold;"><?php echo $_SESSION['majorInfo']['majorName'];?></span></span>
 <span id="address">Current address on file:<br><?php echo $userInfo['addressNumber']." ".$userInfo['addressStreet']."<br>".$userInfo['addressCity'].", ".$userInfo['stateAbbrev']." ".$userInfo['addressZip'];?></span>
 <span id="studentInfo">Current student info:<br><?php ;?></span>
