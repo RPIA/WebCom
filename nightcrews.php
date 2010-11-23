d2fb23bb3ec42762b73284ac7d2ce950a2d82cda
Michael O'Keefe <okeefm@rpi.edu>
2010-11-22 20:09:39 -0500

    Some more night crew stuff

--- nightcrews.php
+++ nightcrews.php
@@ -6,11 +6,11 @@ display_header("Night Crews", "");
 
 if (logged_in()) {
 	// form to change profile info
-	echo "Night Crew information goes here";
+	echo "Night Crew information goes here.";
 }
 
 else if (!logged_in()) {
-  echo "Can't change anything, 'cause you're not logged in";
+  echo "Can't change anything, 'cause you're not logged in.";
 }
 
 display_footer();
