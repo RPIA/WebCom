319e6ebfe2f5a99166ba27dc9f5296687a8c7073
Michael O'Keefe <okeefm@rpi.edu>
2011-01-25 00:44:21 -0500

    Added a function to crews.php

--- global/crews.php
+++ global/crews.php
@@ -1,23 +1,27 @@
 <?php
 
-/*function get_crew_member($date, $position_id) {
+function get_crew_member($date, $position_id) {
   $result = std_query("SELECT * FROM `nightCrews` 
     INNER JOIN `positions` ON `nightCrews'.'positionID`=`crewPositions`.`id`
     INNER JOIN `members` ON `nightCrews`.`memberID`=`users`.`id`
     WHERE `date` = '".quote_smart($date)."' and `positionID` = '".quote_smart($position_id)."'
-    ");
+    "); 
   
   $nightCrewInfo = mysql_fetch_assoc($result);
+  return $nightCrewInfo[userID];
 }
 
 function get_crew($date) {
-} */
+}
 
 function display_crew_member($user_id) {
-
-  $result = std_query("SELECT * FROM `users` WHERE `users`.`id`='".$user_id."'");
+  if ($user_id == -1) echo "&nbsp &nbsp <a href=crews/signup>Sign up</a>";
+  else {
+  	$result = std_query("SELECT * FROM `users` WHERE `users`.`id`='".quote_smart($user_id)."'");
     $userInfo = mysql_fetch_assoc($result);
-  echo "&nbsp;&nbsp;<a href = 'mailto:$userInfo['email']' >'$userInfo['firstName'][0]' '$userInfo['lastName']'</a>&nbsp;&nbsp;";  
+    $firstInit = substr($userInfo[firstName], 0, 1);
+  	echo "&nbsp &nbsp <a href=\"mailto:$userInfo[email]\"> $firstInit. $userInfo[lastName]</a>";
+  }
 }
 
 ?>
